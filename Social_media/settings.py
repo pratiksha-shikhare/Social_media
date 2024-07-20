594dc7bce2dfe6e9671b9d01ecfc1cb0a6d82bbe
pratiksha <pratikshashikhare92@gmail.com>
2024-07-20 20:25:08 +0530

    Update settings of static files

--- Social_media/settings.py
+++ Social_media/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,8 +27,6 @@ SECRET_KEY = 'django-insecure-hjpc51_7$y1x^f$30u9w!1x8y@*aac#j&x&g_hh*8cr6tg0#j0
 DEBUG = True
 
 ALLOWED_HOSTS = []
-TEMPLATES_DIR = BASE_DIR / 'templates'
-STATIC_DIR = BASE_DIR / 'static'
 
 # Application definition
 
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'Social_media.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILEs_DIRS = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
