b86020899346aa6ff3ec695e9c0c2410d6241eaf
pratiksha <pratikshashikhare92@gmail.com>
2024-08-05 22:59:25 +0530

    update login and signup

--- core/views.py
+++ core/views.py
@@ -1,17 +1,21 @@
+# from django.contrib.auth.models import User
+# from django.contrib import messages, auth
+# from django.shortcuts import redirect, render
 from .models import Profile
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.models import User, auth
-from django.contrib import messages
-from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 # Create your views here.
-# @login_required(login_url='signin')
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
-# @login_required(login_url='signin')
+@login_required(login_url='signin')
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
     
@@ -38,13 +42,14 @@ def settings(request):
 
     return render(request, "settings.html", {"user_profile":user_profile})
     
+
 def signup(request):
     if request.method == 'POST':
         username = request.POST.get("username")
         email = request.POST.get("email")
         password = request.POST.get("password")
         password2 = request.POST.get("password2")
-        # print(username, email, password, password2)
+        print(username, email, password, password2)
         if password == password2:
             if User.objects.filter(email=email).exists():
                 messages.info(request, "Email already exists")
@@ -53,39 +58,64 @@ def signup(request):
                 messages.info(request, "Username exists")
                 return redirect('signup')
             else:
-                user = User.objects.create(username=username, email=email, password=password)
+                user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
                 
-                # log user
+                # log user in
                 user_login = auth.authenticate(username=username, password=password)
-                auth.login(request, user_login)
-                
-                # create profile object
-                user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model, id_user=user.id)
-                new_profile.save()
-                return redirect("settings")
+                if user_login is not None:
+                    auth.login(request, user_login)
+                    
+                    # create profile object
+                    user_model = User.objects.get(username=username)
+                    new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                    new_profile.save()
+                    return redirect("settings")
+                else:
+                    messages.info(request, "Authentication failed")
+                    return redirect('signup')
         else:
-            messages.info(request, "Password must match!!")
+            messages.info(request, "Passwords must match!!")
             return redirect('signup')
-            
     else:
         return render(request, 'signup.html')
-    
+
+# def signin(request):
+#     if request.method == "POST":
+#         print("Post is calling")
+#         username = request.POST.get("username")
+#         password = request.POST.get("password")
+#         print(username, password)
+#         user = auth.authenticate(username=username, password=password)
+#         if user is not None:
+#             auth.login(request, user)
+#             print("Loginis working")
+#             # return redirect("/")
+#             return render(request, "index.html")
+#         else:
+#             messages.info(request, "Credentials Invalid")
+#             return redirect("signin.html")
+#     else:
+#         return render(request, "signin.html")
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
 def signin(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
-        print(username, password)
-        user = auth.authenticate(username=username, password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
-            auth.login(request, user)
-            return redirect("/")
+            login(request, user)
+            return redirect("index")
         else:
-            messages.info(request, "Credentials Invalid")
-            return redirect("signin.html")
+            messages.error(request, "Invalid credentials")
+            return redirect("signin")
     else:
         return render(request, "signin.html")
+
 
 login_required(login_url="signin")
 def logout(request):
